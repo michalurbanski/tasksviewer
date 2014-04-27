1ce6215d1727c53f5ea179d5a42fbb0df5a0be98
michalurbanski <michal_urbanski@windowslive.com>
2014-04-27 23:11:53 +0200

    unit tests project added + first initial test in order to confirm that nunit works correctly + nuget packages for nunit

---
+++ TasksViewer/TasksViewer.UnitTests/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksViewer.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            string a = "string";
+            string expected = a;
+
+            Assert.AreEqual(expected, a);
+        }
+    }
+}
