9896e7dfad028c9f629d42943b11a7a25e0b0868
michalurbanski <michal_urbanski@windowslive.com>
2014-07-01 23:39:34 +0200

    ConfigurationVariables class added for values used in application

---
+++ TasksViewer/TasksViewer.UnitTests/ConfigurationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TasksViewer.Configuration;
+
+namespace TasksViewer.UnitTests
+{
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void Get_ConfigurationPath_ShouldReturnCorrectPath()
+        {
+            string path = ConfigurationVariables.ConfigurationFileFullPath;
+
+            Console.WriteLine("Current configuration file path is " + path);
+
+            Assert.IsNotNullOrEmpty(path); 
+        }
+    }
+}
---
+++ TasksViewer/TasksViewer/Configuration/ConfigurationVariables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksViewer.Configuration
+{
+    /// <summary>
+    /// Configuration variables used in application
+    /// </summary>
+    public static class ConfigurationVariables
+    {
+        private static readonly string _configurationFilePath = @"Configuration\configuration.json";
+
+        public static string ConfigurationFileFullPath 
+        {
+            get
+            {
+                // TODO: 2014-07-01 for now simple assumption that program is in \bin\Debug
+                string currentDirectory = Directory.GetCurrentDirectory();
+                currentDirectory = currentDirectory.Replace(@"\bin\Debug", "");
+
+                return Path.Combine(currentDirectory, _configurationFilePath);
+            }        
+        }
+
+    }
+}
