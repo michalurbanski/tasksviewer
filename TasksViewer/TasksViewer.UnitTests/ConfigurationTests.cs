116fce90901ee0a3d3642c1791207ec8652422a2
michalurbanski <michal_urbanski@windowslive.com>
2014-07-02 00:22:14 +0200

    unit tests for configuration variables class

--- TasksViewer/TasksViewer.UnitTests/ConfigurationTests.cs
+++ TasksViewer/TasksViewer.UnitTests/ConfigurationTests.cs
@@ -8,17 +8,52 @@ using TasksViewer.Configuration;
 
 namespace TasksViewer.UnitTests
 {
+    /// <summary>
+    /// Tests for configuration class
+    /// </summary>
     [TestFixture]
     public class ConfigurationTests
     {
-        [Test]
-        public void Get_ConfigurationPath_ShouldReturnCorrectPath()
+        ConfigurationVariables _configuration; 
+
+        [SetUp]
+        public void Initialize()
         {
-            string path = ConfigurationVariables.ConfigurationFileFullPath;
+            _configuration = new ConfigurationVariables(); 
+        }
+
+        [Test]
+        public void Get_DefaultConfigurationPath_ShouldReturnCorrectPath()
+        {
+            string path = _configuration.ConfigurationFileFullPath;
 
             Console.WriteLine("Current configuration file path is " + path);
 
             Assert.IsNotNullOrEmpty(path); 
+        }
+
+        [Test]
+        public void Get_ConfigurationPath_EmptyPath_ShouldReturnStringEmpty()
+        {
+            // set empty string as configuration file path
+            string configurationFilePath = string.Empty;
+            _configuration.ConfigurationFileFullPath = configurationFilePath; 
+
+            string result = _configuration.ConfigurationFileFullPath;
+
+            Assert.IsEmpty(result);
+        }
+
+        [Test]
+        public void Set_ConfigurationFilePath_FullPathShouldBeValid()
+        {
+            string fileName = "custompath";
+
+            _configuration.ConfigurationFileFullPath = fileName;
+
+            string result = _configuration.ConfigurationFileFullPath;
+
+            Assert.True(result.Contains(fileName));
         }
     }
 }
--- TasksViewer/TasksViewer/Configuration/ConfigurationVariables.cs
+++ TasksViewer/TasksViewer/Configuration/ConfigurationVariables.cs
@@ -10,20 +10,30 @@ namespace TasksViewer.Configuration
     /// <summary>
     /// Configuration variables used in application
     /// </summary>
-    public static class ConfigurationVariables
+    /// <remarks>Not made as static class due to required unit testing</remarks>
+    public class ConfigurationVariables
     {
-        private static readonly string _configurationFilePath = @"Configuration\configuration.json";
+        private string _configurationFilePath = @"Configuration\configuration.json";
 
-        public static string ConfigurationFileFullPath 
+        public string ConfigurationFileFullPath 
         {
             get
             {
+                if (string.IsNullOrEmpty(_configurationFilePath))
+                {
+                    return string.Empty; 
+                }
+
                 // TODO: 2014-07-01 for now simple assumption that program is in \bin\Debug
                 string currentDirectory = Directory.GetCurrentDirectory();
                 currentDirectory = currentDirectory.Replace(@"\bin\Debug", "");
 
                 return Path.Combine(currentDirectory, _configurationFilePath);
-            }        
+            }
+            set // setter allows to test various test cases regarding file presence and correctness
+            {
+                _configurationFilePath = value; 
+            }
         }
 
     }
